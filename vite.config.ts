242cfcb39e597cdf3e04a9a648f387a5f3dff604
Zachary Dodge <zacharysdodge@gmail.com>
2024-10-21 22:54:59 -0700

    feat: PWA for web

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { checker } from 'vite-plugin-checker';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export const plugins = [
   checker({
@@ -13,6 +14,46 @@ export const plugins = [
   }),
   nodePolyfills(),
   react(),
+  VitePWA({
+    manifest: {
+      description: 'AI thread app',
+      icons: [
+        {
+          sizes: '64x64 32x32 24x24 16x16',
+          src: '/favicon.ico',
+          type: 'image/x-icon',
+        },
+        {
+          sizes: '192x192',
+          src: '/icon192.png',
+          type: 'image/png',
+        },
+        {
+          purpose: 'maskable',
+          sizes: '192x192',
+          src: '/icon192.png',
+          type: 'image/png',
+        },
+        {
+          sizes: '512x512',
+          src: '/icon512.png',
+          type: 'image/png',
+        },
+        {
+          purpose: 'maskable',
+          sizes: '512x512',
+          src: '/icon512.png',
+          type: 'image/png',
+        },
+      ],
+      name: 'AI Threads',
+      short_name: 'Threads',
+    },
+    registerType: 'autoUpdate',
+    workbox: {
+      maximumFileSizeToCacheInBytes: 4194304,
+    },
+  }),
 ];
 
 /**
@@ -20,5 +61,5 @@ export const plugins = [
  */
 export default defineConfig({
   plugins,
-  root: 'src/renderer',
+  root: process.env.NODE_ENV === 'production' ? undefined : 'src/renderer',
 });
